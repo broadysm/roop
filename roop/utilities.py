44d776fa2d2238fb26094a4232accaec2531c517
S. M. Yoon <broadysm@nate.com>
2023-08-01 01:49:48 +0900

    Update utilities.py
    Conversion from [0~100] to [0~50] has a problem.
    
    You should remove +1 form the conversion equation.

--- roop/utilities.py
+++ roop/utilities.py
@@ -51,7 +51,7 @@ def extract_frames(target_path: str, fps: float = 30) -> bool:
 def create_video(target_path: str, fps: float = 30) -> bool:
     temp_output_path = get_temp_output_path(target_path)
     temp_directory_path = get_temp_directory_path(target_path)
-    output_video_quality = (roop.globals.output_video_quality + 1) * 51 // 100
+    output_video_quality = (roop.globals.output_video_quality) * 51 // 100
     commands = ['-hwaccel', 'auto', '-r', str(fps), '-i', os.path.join(temp_directory_path, '%04d.' + roop.globals.temp_frame_format), '-c:v', roop.globals.output_video_encoder]
     if roop.globals.output_video_encoder in ['libx264', 'libx265', 'libvpx']:
         commands.extend(['-crf', str(output_video_quality)])
